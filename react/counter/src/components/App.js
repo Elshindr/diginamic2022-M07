726e6ceaf1b28c4e3f995af0c75c967e2d414b08
yvandouenel <y.douenel@coopernet.fr>
2023-08-16 15:48:40 +0200

    React avec un state et des props.

--- react/counter/src/components/App.js
+++ react/counter/src/components/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import Counter from './Counter';
+import { useState } from 'react';
 
-function App() {
+function App({title}) {
+  // Destructuring pour gérer le state
+  const [counter, setCounter] = useState(0);
+  /* const maVariable = useState(0)[0];
+  const setMaVariable = useState(0)[1]; */
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+  const decrement = () => {
+    setCounter(counter - 1);
+  }
   return (
     <div className="App container">
       <h1>Compteur</h1>
+      <button 
+      onClick={increment}
+      className="btn btn-warning me-3">Incrémenter</button>
+      <button 
+      onClick={decrement}
+      className="btn btn-warning me-3">Decrémenter</button>
+      <Counter counter={counter} />
     </div>
   );
 }
---
+++ react/counter/src/components/Counter.js
@@ -0,0 +1,7 @@
+const Counter = (props) => {
+  return (
+    <button className="btn btn-success">{props.counter}</button>
+  );
+}
+
+export default Counter;
