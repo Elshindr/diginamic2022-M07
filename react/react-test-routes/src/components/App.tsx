3b068f7b2c52dfa08b64af8b0f8102f730176d68
yvandouenel <y.douenel@coopernet.fr>
2023-08-22 11:38:03 +0200

    Début de react router.

---
+++ react/react-test-routes/src/components/App.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+     <header>
+      <h1>Header commun</h1>
+     </header>
+     <main>
+      <Outlet />
+     </main>
+    </div>
+  );
+}
+
+export default App;
